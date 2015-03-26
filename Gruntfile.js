db0be5145d9bbdab943b1565d0b74df03468e999
David Von Lehman <david.vonlehman@gmail.com>
2015-03-26 13:26:02 -0700

    Fixing up some paths and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,12 +17,21 @@ module.exports = function(grunt) {
           'dist/app.min.css': ['node_modules/normalize-css/normalize.css', 'css/main.css']
         }
       }
+    },
+    copy: {
+      dist: {
+        files: [
+          {src: 'index.html', dest: 'dist/index.html'},
+          {expand: true, src: ['images/**'], dest: 'dist/'}
+        ]
+      }
     }
   });
 
-  grunt.registerTask('build', ['jshint', 'uglify', 'cssmin']);
+  grunt.registerTask('build', ['jshint', 'copy:dist', 'uglify', 'cssmin']);
 
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
